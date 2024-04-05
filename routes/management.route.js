1aa603a0cc08b08e58e9d88be903043bc7e56bfa
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-04-06 03:03:39 +0530

    feat: user management completed

--- routes/management.route.js
+++ routes/management.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const middleware = require("./middleware");
 
 router.use("/auth", require("./management/auth.route"))
+router.use("/users", middleware.managementAuth, require("./management/user.route"))
 router.use("/senders", middleware.managementAuth, require("./management/sender.route"))
 router.use("/policies", middleware.managementAuth, require("./management/policy.route"))
 
---
+++ routes/management/user.route.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const prisma = require("../../db").getInstance();
+
+// Get all users
+router.get("/", async (req, res) => {
+    const users = await prisma.user.findMany({
+        select: {
+            id: true,
+            email: true,
+            name: true
+        }
+    });
+    res.json(users);
+})
+
+// Create user
+router.post("/", async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) return res.status(400).json({
+        error: "Missing required fields"
+    });
+    let passwordHash = await bcrypt.hash(password, 10);
+    await db.user.create({
+        data: {
+            email: email,
+            passwordHash: passwordHash,
+            name: name
+        }
+    })
+    res.json({
+        message: "User created"
+    })
+})
+
+// Delete user
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    if (id === req.user.id) {
+        return res.status(400).json({
+            error: "Cannot delete yourself"
+        });
+    }
+    const user = await prisma.user.delete({
+        where: {
+            id: parseInt(id)
+        }
+    });
+    res.json(user);
+})
+
+module.exports = router
